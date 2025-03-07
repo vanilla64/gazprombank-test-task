21256a930b4a898f24b56a7a37d51283452bfcee
Sergey <vanillaen@yandex.ru>
2025-03-07 15:28:38 +0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Gazprombank Test Task</h1>
+    </div>
+  );
+}
+
+export default App;
